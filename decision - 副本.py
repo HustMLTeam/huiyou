7b571d8d66ea49d19afe9d9c51264b183d965c36
zhangdanzhu <1240864213@qq.com>
2016-05-05 23:15:05 +0800

    update

--- decision - 副本.py
+++ decision - 副本.py
@@ -4,6 +4,9 @@ import numpy as np
 from sklearn.cluster import KMeans
 
 
+#除了中心还有大小要考虑
+
+
 def max_cover(n):
     def dec(X):
         cluster = KMeans(n, random_state=42)
@@ -18,30 +21,17 @@ def max_cover(n):
         return result
     return dec
 
-def min_cover(n):
+def Overlapping_cover(n):
     def dec(X):
         cluster = KMeans(n, random_state=42)
         y = cluster.fit_predict(X)
         result = np.array([]).reshape(0, 4)
         for i in range(n):
-            y_start = X[y == i][:, 0].max()
-            y_end = X[y == i][:, 1].min()
-            x_start = X[y == i][:, 2].max()
-            x_end = X[y == i][:, 3].min()
+            y_start = X[y == i][:, 0].min()
+            y_end = X[y == i][:, 1].max()
+            x_start = X[y == i][:, 2].min()
+            x_end = X[y == i][:, 3].max()
             result = np.vstack((result, (y_start, y_end, x_start, x_end)))
         return result
     return dec
 
-def average_cover(n):
-    def dec(X):
-        cluster = KMeans(n, random_state=42)
-        y = cluster.fit_predict(X)
-        result = np.array([]).reshape(0, 4)
-        for i in range(n):
-            y_start = X[y == i][:, 0].sum() / X[y == i].shape(0)
-            y_end = X[y == i][:, 1].sum() / X[y == i].shape(0)
-            x_start = X[y == i][:, 2].sum() / X[y == i].shape(0)
-            x_end = X[y == i][:, 3].sum() / X[y == i].shape(0)
-            result = np.vstack((result, (y_start, y_end, x_start, x_end)))
-        return result
-    return dec
