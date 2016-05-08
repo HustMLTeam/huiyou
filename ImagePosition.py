5df27503e5c175ee8437d73098c5c33a38466383
DrStitch <467738357@qq.com>
2016-05-08 04:42:50 -0500

    Update ImagePosition.py

--- ImagePosition.py
+++ ImagePosition.py
@@ -1,13 +1,13 @@
 from classifier import Svm
 from feature_extractor import Sift
 from slide_window import SlideWindow
-from decision import max_cover
+from decision import max_cover, average_cover
 import cv2
 
 def getTubePos(img):
     feature_extractor = Sift().tube()
     classifier = Svm().tube()
-    decision = max_cover(2)
+    decision = average_cover(2)
     s_win = SlideWindow(feature_extractor, classifier, decision, width_min=20,
                         width_max=45, width_inc=10, height_min=210,
                         height_max=250, height_inc=10, x_step=3, y_step=10)
@@ -17,7 +17,7 @@ def getTubePos(img):
 def getWindowPos(tube, n):
     feature_extractor = Sift().window()
     classifier = Svm().window()
-    decision = max_cover(n)
+    decision = average_cover(n)
     s_win = SlideWindow(feature_extractor, classifier, decision, width_min=13,
                         width_max=27, width_inc=3, height_min=25,
                         height_max=40, height_inc=3, x_step=3, y_step=4,
